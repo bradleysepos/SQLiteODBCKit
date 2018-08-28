9727432fa4290e2150d2583f69df0bc13c60dc19
Bradley Sepos <bradley@bradleysepos.com>
2018-08-27 20:34:19 -0400

    Add Xcode project and external build script.

---
+++ SQLiteODBCKit/SQLiteODBCKit.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteODBCKit.h
+//  SQLiteODBCKit
+//
+//  Created by Bradley Sepos on 8/27/18.
+//  Copyright © 2018 Bradley Sepos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SQLiteODBCKit.
+FOUNDATION_EXPORT double SQLiteODBCKitVersionNumber;
+
+//! Project version string for SQLiteODBCKit.
+FOUNDATION_EXPORT const unsigned char SQLiteODBCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteODBCKit/PublicHeader.h>
+
+
